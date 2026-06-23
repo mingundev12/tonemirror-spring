b70c94c354b23b70de9ea302d501e5c773085be0
mingundev12 <mingundev12@gmail.com>
2026-06-23 09:41:46 +0900

    init project

---
+++ src/main/java/com/tonemirror/TonemirrorSpringApplication.java
@@ -0,0 +1,13 @@
+package com.tonemirror;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TonemirrorSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TonemirrorSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tonemirror/TonemirrorSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tonemirror;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TonemirrorSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
