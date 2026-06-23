d3ec5ee3a9f3606787c292468bc91e4f7b19de13
mingundev12 <mingundev12@gmail.com>
2026-06-23 17:01:42 +0900

    fix : pageText -> pageContext 클래스 명칭 및 필드명 수정

--- src/main/java/com/tonemirror/entity/common/PageContext.java
+++ src/main/java/com/tonemirror/entity/common/PageContext.java
@@ -6,14 +6,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "page_text")
+@Table(name = "page_context")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class PageText {
+public class PageContext {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "page_text_id")
-    private Long pageTextId;
+    @Column(name = "page_context_id")
+    private Long pageContextId;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "page_id", nullable = false)
@@ -23,12 +23,12 @@ public class PageText {
     @JoinColumn(name = "lang_id", nullable = false)
     private Lang lang;
 
-    @Column(name = "data_set", columnDefinition = "JSON")
-    private String dataSet;
+    @Column(name = "context", columnDefinition = "JSON")
+    private String context;
 
-    public PageText(Page page, Lang lang, String dataSet) {
+    public PageContext(Page page, Lang lang, String context) {
         this.page = page;
         this.lang = lang;
-        this.dataSet = dataSet;
+        this.context = context;
     }
 }
