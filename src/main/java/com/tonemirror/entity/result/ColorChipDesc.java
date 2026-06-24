d5c76e7c95b3b7299be1fabb6fd58cd36277f517
mingundev12 <mingundev12@gmail.com>
2026-06-24 09:45:54 +0900

    feat : 컬러 칩 언어별 테이블 구현

---
+++ src/main/java/com/tonemirror/entity/result/ColorChipDesc.java
@@ -0,0 +1,46 @@
+package com.tonemirror.entity.result;
+
+import com.tonemirror.entity.common.Lang;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(
+        name = "color_chip_desc",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "uk_color_lang",
+                        columnNames = {"color_id", "lang_id"}
+                )
+        } // 복합 Unique Key 설정
+)
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ColorChipDesc {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "color_chip_desc_id")
+    private Long colorChipDescId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "color_id", nullable = false)
+    private ColorChip color;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "lang_id", nullable = false)
+    private Lang lang;
+
+    @Column(name = "color_name", length = 50)
+    private String colorName;
+
+    public ColorChipDesc(ColorChip color, Lang lang, String colorName) {
+        if (color == null || lang == null || colorName == null) {
+            throw new IllegalArgumentException("color, lang, and colorName are mandatory");
+        }
+        this.color = color;
+        this.lang = lang;
+        this.colorName = colorName;
+    }
+}
