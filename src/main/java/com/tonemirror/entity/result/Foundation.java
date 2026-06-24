a5448e50ac1dd0111b6ab7ca130ff2e595460e9f
mingundev12 <mingundev12@gmail.com>
2026-06-24 13:38:34 +0900

    feat : 파운데이션 마스터 테이블 및 언어별 데이터셋 테이블 구현

---
+++ src/main/java/com/tonemirror/entity/result/Foundation.java
@@ -0,0 +1,32 @@
+package com.tonemirror.entity.result;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "foundation")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Foundation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "foundation_id")
+    private Long foundationId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "tone_id")
+    private Tone tone;
+
+    @Column(name = "label", nullable = false, length = 20)
+    private String label;
+
+    @Column(name = "brightness", nullable = false)
+    private Integer brightness;
+
+    @Column(name = "hex", nullable = false, length = 7)
+    private String hex;
+
+    // todo : 서비스 도메인에 맞춰 생성자 구성할 것
+}
